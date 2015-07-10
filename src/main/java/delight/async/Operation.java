3940b776d8d323386a63497100b664a5742e229e
mxro <mxro@nowhere.com>
2015-07-10 16:21:22 +1000

    Implementing key value database Autocommit Fri Jul 10 16:21:22 EST 2015

--- src/main/java/delight/async/Operation.java
+++ src/main/java/delight/async/Operation.java
@@ -1,14 +1,16 @@
 package delight.async;
 
 import delight.async.callbacks.ValueCallback;
+import delight.functional.Closure;
 
 /**
  * The definition of an asynchronous operation with no parameters.
  *
  * @param <Result>
  */
-public interface Operation<Result> {
+public interface Operation<Result> extends Closure<ValueCallback<Result>> {
 
+    @Override
     public void apply(ValueCallback<Result> callback);
 
 }
